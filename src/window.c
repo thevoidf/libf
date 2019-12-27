38e224afe92fd35d6ddf848bfb9029ac57df8dd0
Void <thevoidf@gmail.com>
2019-12-27 15:51:44 +0400

    input handling

--- src/window.c
+++ src/window.c
@@ -2,11 +2,21 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "glad/glad.h"
-#include "glfw/glfw3.h"
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 
 lowg_window_t* lowg_window_new(char* title, int width, int height)
 {
+  int i;
+  for (i = 0; i < MAX_KEYS; i++) {
+    keys[i] = false;
+    key_state[i] = false;
+    key_typed[i] = false;
+  }
+  for (i = 0; i < MAX_BUTTONS; i++) {
+    mouse_buttons[i] = false;
+  }
+
   if (!glfwInit()) {
     printf("Failed to init glfw");
     return NULL;
@@ -16,7 +26,7 @@ lowg_window_t* lowg_window_new(char* title, int width, int height)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  lowg_window_t* w = (lowg_window_t*) malloc(sizeof(lowg_window_t));
+  lowg_window_t* w = malloc(sizeof(lowg_window_t));
 
   w->window = glfwCreateWindow(width, height, title, NULL, NULL);
   if (!w->window) {
@@ -25,6 +35,12 @@ lowg_window_t* lowg_window_new(char* title, int width, int height)
   }
 
   glfwMakeContextCurrent(w->window);
+
+  glfwSetKeyCallback(w->window, lowg_window_key_callback);
+  glfwSetCursorPosCallback(w->window, lowg_window_mouse_position_callback);
+  glfwSetMouseButtonCallback(w->window, lowg_window_mouse_button_callback);
+  glfwSetFramebufferSizeCallback(w->window, lowg_window_buffer_size_callback);
+
   if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
     printf("Failed to init OpenGL\n");
     return NULL;
@@ -49,10 +65,63 @@ void lowg_window_clear(float r, float g, float b)
 
 void lowg_window_update(lowg_window_t* w)
 {
-  GLenum error = glGetError();
-  if (error != GL_NO_ERROR)
-    printf("OpenGL Error: %d\n", error);
+  // BETTER HANDLEING FFS
+  /* GLenum error = glGetError(); */
+  /* if (error != GL_NO_ERROR) */
+  /*   printf("OpenGL Error: %d\n", error); */
 
   glfwSwapBuffers(w->window);
   glfwPollEvents();
+  lowg_window_update_keys();
+}
+
+void lowg_window_update_keys()
+{
+  int i;
+  for (i = 0; i < MAX_KEYS; i++)
+    key_typed[i] = keys[i] && !key_state[i];
+  for (i = 0; i < MAX_KEYS; i++)
+    key_state[i] = keys[i];
+}
+
+bool lowg_window_is_key_pressed(unsigned int key_code)
+{
+  if (key_code > MAX_KEYS)
+    return false;
+  return keys[key_code];
+}
+
+bool lowg_window_is_key_down(unsigned int key_code)
+{
+  if (key_code > MAX_KEYS)
+    return false;
+  return key_typed[key_code];
+}
+
+bool lowg_window_is_mouse_button_pressed(unsigned int button_code)
+{
+  if (button_code > MAX_BUTTONS)
+    return false;
+  return mouse_buttons[button_code];
+}
+
+void lowg_window_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  keys[key] = action != GLFW_RELEASE;
+}
+
+void lowg_window_mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+  mouse_buttons[button] = action != GLFW_RELEASE;
+}
+
+void lowg_window_mouse_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+  mousex = xpos;
+  mousey = ypos;
+}
+
+void lowg_window_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
 }
--- src/window.h
+++ src/window.h
@@ -1,6 +1,11 @@
 #ifndef LOWG_WINDOW_H
 #define LOWG_WINDOW_H
 
+#include <stdbool.h>
+
+#define MAX_KEYS    1024
+#define MAX_BUTTONS   32
+
 typedef struct GLFWwindow GLFWwindow;
 
 typedef struct {
@@ -10,9 +15,26 @@ typedef struct {
   GLFWwindow* window;
 } lowg_window_t;
 
+bool keys[MAX_KEYS];
+bool key_state[MAX_KEYS];
+bool key_typed[MAX_KEYS];
+bool mouse_buttons[MAX_BUTTONS];
+double mousex, mousey; 
+
 lowg_window_t* lowg_window_new(char* title, int width, int height);
 int lowg_window_should_close();
 void lowg_window_clear(float r, float g, float b);
 void lowg_window_update();
+
+void lowg_window_update_keys();
+
+bool lowg_window_is_key_pressed(unsigned int key_code);
+bool lowg_window_is_key_down(unsigned int key_code);
+bool lowg_window_is_mouse_button_pressed(unsigned int button_code);
+
+void lowg_window_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void lowg_window_mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void lowg_window_mouse_position_callback(GLFWwindow* window, double xpos, double ypos);
+void lowg_window_buffer_size_callback(GLFWwindow* window, int width, int height);
 
 #endif
